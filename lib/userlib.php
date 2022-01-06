fe9194cc7f2f62f23444beab342047bd784cd1f5
awaisOptimistic <awais@of.edu.au>
2022-01-06 16:52:25 +1100

    Handled if wanted to start a new enrolment

--- lib/userlib.php
+++ lib/userlib.php
@@ -1,8 +1,8 @@
 <?php
 require_once ('lib.php');
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include '../config.php';
     session_start();
     if (isset($_POST["newcourse"])){
@@ -23,7 +23,7 @@ include '../config.php';
         $rownew   = $stmtnew->fetch(PDO::FETCH_ASSOC);
 
         if($rownew['enrolForm']==NULL){
-        //    //update course name in course table
+            //update course name in course table
             $sql = "UPDATE `courses` SET `course`=? WHERE id=?";
             $stmt= $pdo->prepare($sql);
             $stmt->execute([$_POST["newcourse"],$rownew["courseid"]]);
@@ -31,28 +31,54 @@ include '../config.php';
             $sql = "UPDATE `user` SET `courses`=? WHERE id=?";
             $stmt= $pdo->prepare($sql);
             $stmt->execute([$_POST["newcourse"],$userId]);
+
         }else{
-            /***Insert Query***/
+            // need userid,uqid
+            $uqid = get_uqid($userId);
+            $userId=$_SESSION['userid'];
+
+            //add in course table
             $queryCourse  = "INSERT INTO `courses`( `userid`, `course`) VALUES (:userid,:course)";
             $stmtCourse = $pdo->prepare($queryCourse);
             $stmtCourse->bindParam('userid', $userId, PDO::PARAM_STR);
             $stmtCourse->bindParam('course', $_POST["newcourse"], PDO::PARAM_STR);
             $stmtCourse->execute();
 
-            $sql = "UPDATE `user` SET `courses`=? WHERE id=?";
-            $stmt= $pdo->prepare($sql);
-            $stmt->execute([$_POST["newcourse"],$userId]);
+            ////get course id
+            $querynew = "SELECT id FROM `courses` where `userid`=:userid order by id desc limit 1";
+            $stmtnew = $pdo->prepare($querynew);
+            $stmtnew->bindParam('userid', $userId, PDO::PARAM_STR);
+            $stmtnew->execute();
+            $rownew   = $stmtnew->fetch();
+            $courseId=$rownew["id"];
 
-            $uqid = get_uqid($userId);
-            $query5  = "INSERT INTO `of_enrolment`(`usrid`, `std_id`)VALUES (:usrid,:uqid )";
+
+            //add new enrolment record
+            $query5  = "INSERT INTO `of_enrolment`(`usrid`, `std_id`,`courseid`)VALUES (:usrid,:uqid,:courseid )";
             $stmt5 = $pdo->prepare($query5);
             $stmt5->bindParam('usrid', $userId, PDO::PARAM_STR);
             $stmt5->bindParam('uqid', $uqid, PDO::PARAM_STR);
+            $stmt5->bindParam('courseid', $courseId, PDO::PARAM_STR);
             $stmt5->execute();
+
+            //get enrolment id
+            $querynew1 = "SELECT id FROM `of_enrolment` where `usrid`=:userid AND `courseid`=:courseid ";
+            $stmtnew1 = $pdo->prepare($querynew1);
+            $stmtnew1->bindParam('userid', $userId, PDO::PARAM_STR);
+            $stmtnew1->bindParam('courseid', $courseId, PDO::PARAM_STR);
+            $stmtnew1->execute();
+            $rownew1   = $stmtnew1->fetch();
+            $enrolmentId=$rownew1["id"];
+
+            //update user table
+            $sql = "UPDATE `user` SET `courses`=?,`enrolmentId`=? WHERE id=?";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute([$_POST["newcourse"],$enrolmentId,$userId]);
+
         }
 
 
-        echo ("YES");
+       // echo ("YES");
     }
 
 if (isset($_POST["oldpass"])){
